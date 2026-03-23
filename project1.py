3bab2266567a43a2bbaf13679f9d2bf48954fa25
Ana <anaisabelfp2003@gmail.com>
2026-03-23 00:08:26 +0000

    exatamente como foi entregue, pos mais testes e passei-os para o fim, alt. T8 e T9 por causa da prof querer que usemos SEMPREEEE que possivel funcoes anteriores

--- project1.py
+++ project1.py
@@ -1,4 +1,5 @@
-## TODO: Include here the Python standard library modules you need.
+import re
+from collections import Counter
 #import Biopython #just to use for checks
 
 ## Part I
@@ -7,43 +8,35 @@
 
 def readFASTA(file: str):
     with open(file, 'r',encoding='utf-8-sig') as f:
-        d = {} # da para pre definir como vai ser? nao, fazer so depois
+        d = {}
         ID = None
         seq = ""
         desc= ""
         for line in f:
             line = line.rstrip("\n")
-            if line.startswith('>'): # header
-                if ID is not None: # ja lemos sequencia daqui
+            if line.startswith('>'):
+                if ID is not None:
                     d[ID] = (desc, seq)
-                #else: 1 header ou qualquer novo
-                #ate primeiro espaço
-                #index = line.find(" ") nao usar porque caso tenha so a parte do idseq sem descriçao, nao ha espaço da -1
                 header = line[1:].strip()
-                spl= header.split(maxsplit=1) #primeiro espaço
+                spl= header.split(maxsplit=1)
                 ID = spl[0]
                 if len(spl) > 1: desc = spl[1]
                 else: desc=""
                 seq = ""
-            else: # sequence
-                seq += line # ja tirou-se quebra de linha
-                # fasta files nao tem espaços e outras coisas
-            #parse last seq
-            if ID is not None: # ja lemos sequencia daqui
+            else:
+                seq += line
+            if ID is not None:
                     d[ID] = (desc, seq)
     return d
-
-#print(readFASTA('data_sequences/CY043485.1.fasta'))
-#print(readFASTA('test.fasta'))
 
 ### T2
 
 def seqComposition(seq, type):
     if type == "DNA":
-        dict = {'A':0, 'T':0, 'C':0, 'G':0}
+        dict = {'A':0, 'C':0, 'G':0, 'T':0} # ordem de exemplo
     
     elif type == "RNA":
-        dict = {'A':0, 'U':0, 'C':0, 'G':0}
+        dict = {'A':0, 'C':0, 'G':0, 'U': 0}
     
     elif type == "PROTEIN":
         dict = {'A':0, 'R':0, 'N':0, 'D':0, 'C':0,
@@ -57,16 +50,13 @@ def seqComposition(seq, type):
             dict[nuc] += 1
     
     return dict
-#print(seqComposition('AGCCGTACGG', 'DNA'))
-#print(seqComposition('AGCCGTACGG', 'RNA'))
 
 def dnaGCcontent(seq):
    dict = seqComposition(seq, "DNA")
    cg = dict['C'] + dict['G']
-   total = len(seq)
-   return str(round(cg/total * 100)) + ('%'+ " GC composition")
+   total = dict['C'] + dict['G'] + dict['T'] + dict['A'] # tal como seqcomp ignorar chars maus
+   return str(round((cg/total * 100), 2)) + ('%'+ " GC composition") 
 
-#print(dnaGCcontent("AGCCGTACGG"))
 ## Parte II
 
 ### T3
@@ -80,10 +70,6 @@ def transcribeDNA2RNA(seq):
             rna_seq+=nuc
     return rna_seq     
 
-dna_seq = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"
-rna_seq = transcribeDNA2RNA(dna_seq)
-print("DNA:", dna_seq)
-print("RNA:", rna_seq)
 ### T4
 
 def translateRNA2protein(seq):
@@ -95,39 +81,28 @@ def translateRNA2protein(seq):
             line.rstrip("\n")
             temp =line.split()
             temp2 = temp[-1]
-            genetic_code.append(temp2) # AAs; start; base_1; base_2; base3;
+            genetic_code.append(temp2)
         AAs = genetic_code[0]
         base_1 = genetic_code[2]
         base_2 = genetic_code[3]
         base_3 = genetic_code[4]
         for i in range(0, len(base_1)-1):
             dict[base_1[i]+base_2[i]+base_3[i]] = AAs[i]
-        for j in range(0, len(seq), 3):
+        for j in range(0, len(seq)-2, 3):
             codon = str(seq[j] + seq[j+1] + seq[j+2])
             if codon in dict.keys():
                 protein += dict[codon]
         return protein
-    
-rna_seq = "AUGGCCAUUGUAAUGGGCCGCUGAAAGGGUGCCCGAUAG"
-protein_seq = translateRNA2protein(rna_seq)
-print("RNA:", rna_seq)
-print("Protein:", protein_seq)
-
 
 ### T5
 
 def reverseComplement(sequence : str):
-    #complement:
     dna_complement = {'A': 'T', 'C':'G', 'T': 'A', 'G':'C'}
     dna_com = []
-    for base in sequence: #0(n)
+    for base in sequence:
         if base in dna_complement.keys():
             dna_com.append(dna_complement[base])
-    return "".join(dna_com[::-1]) # or use list.reverse(); reversed()<-iterator or use a loop
-                #join -> O(n)
-dna_seq = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCGATAG"
-rev_complement = reverseComplement(dna_seq)
-print("Reverse complement:", rev_complement)
+    return "".join(dna_com[::-1])
 
 ## Parte III
 
@@ -136,92 +111,66 @@ print("Reverse complement:", rev_complement)
 def findMotif(dna_sequence : str, motif :str):
     step = len(motif)
     positions = []
-    find = []
-    # most be a better way to do this...
-    for i in range(0, len(dna_sequence) - step + 1):
-        find.append(dna_sequence[i])
-        if len(find) == 3:
-            if "".join(find) == motif:
-                positions.append(i - 2)
-            find = find[1:]
+    for i in range(len(dna_sequence)-step + 1):
+        if dna_sequence[i:i+step] == motif:
+            positions.append(i)
+
     return positions
 
-dna_seq = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"
-motif = "GCC"
-positions = findMotif(dna_seq, motif)
-#print(positions)
-
 ### T7
-from collections import Counter
+
+
 def mostFrequentKMotifs(dna_sequence: str, k : int):
     motifs = []
     result = []
-    for b in range(0, len(dna_sequence) -k +1): #o(n)
-            motifs.append(dna_sequence[b: b + k]) #O(k) ?
+    for b in range(0, len(dna_sequence) -k +1):
+            motifs.append(dna_sequence[b: b + k])
+    if not motifs:
+        return []
     most_motifs = Counter(motifs)
     max_freq = most_motifs.most_common(1)[0][1]
     for i,y in most_motifs.items():
         if y == max_freq:
             result.append((i,y))
+    if len(result) == 1:
+        return result[0]
     return result
-
-dna_seq = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"
-dna_seq2 = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCGATAG"
-k = 3
-kmers2 = mostFrequentKMotifs(dna_seq2, k)
-kmers = mostFrequentKMotifs(dna_seq, k)
-#print(kmers)
-#print(kmers2)
 
 ## Parte IV
 
 ### T8
 
 def highestGC(file):
-    dict = readFASTA(file)
-    ID = list(dict.keys())
-    CG_list = []
-    seqs = []
-    for _, y in dict.values():
-        seqs.append(y)
-    for seq in seqs:
-        type_count = Counter(seq)
-        cg = type_count['C'] + type_count['G']
-        total = len(seq)
-        CG_list.append(round(cg/total * 100))
-    i = CG_list.index(max(CG_list))
-    return (ID[i], CG_list[i])
-#highestGC("data_sequences/J02459.1.fasta")
-#print(highestGC("data_sequences/J02459.1.fasta"))
+    fasta = readFASTA(file)
+    max_gc = -1
+    max_id = None
+    for id, (_, seq) in fasta.items():
+        comp = seqComposition(seq, "DNA")
+        cg = comp['C'] + comp['G']
+        total = sum(comp.values())
+        if total == 0:
+            continue
+        gc_percent = round((cg / total) * 100)
+        if gc_percent > max_gc:
+            max_gc = gc_percent
+            max_id = id
+    return (max_id, max_gc)
+
 ### T9
 
-#trocar para DNA
 def compositionMatrix(file):
-    dict = readFASTA(file)
-    ID = list(dict.keys())
-    dict_interno = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
-    lis =[]
-    result={}
-    for _, y in dict.values():
-        for i in range(len(y)):
-            if y[i] in dict_interno.keys():
-                dict_interno[y[i]] += 1
-        lis.append(dict_interno)
-    
-    for l in range(0, len(ID)):
-        result[ID[l]] = lis[l]
+    fasta = readFASTA(file)
+    result = {}
+    for id, (_, seq) in fasta.items():
+        comp = seqComposition(seq, "DNA")
+        result[id] = comp
     return result
-#print(compositionMatrix("test.fasta"))
 
 ### T10
-# considerar "proteins" na verdade se for RNA tambem vai tratar so considerar ATCGN!
-import re
+
+
 def indexSpeciesGC (fasta: dict):
-    out_dict = {} #tipo dict[][] [species][(ids: set(identifiers), 'gc_medio)]
-    # nao é preciso uma lista para cada passa/info
-    #recolher toda a info necessaria de fasta de uma so vez:
-    
-    # falta por o id certo é preciso que seja um set antes de se começara indexar...
+    out_dict = {}
 
     for id, (description, seq) in fasta.items():
         found = re.search(r"\[(.*?)\]", description)
@@ -229,37 +178,165 @@ def indexSpeciesGC (fasta: dict):
             specie = found.group(1)
         else:
             specie = "Unknown"
-        if specie not in out_dict: #new key
+        if specie not in out_dict:
             out_dict[specie] = {'ids': set(), 'gc_medio': 0, 'count': 0}
-        out_dict[specie]['ids'].add(id) # ja sabe que é set()
-# gc_count é None caso tenha algum caracter alem de `A`, `T`, `C`, `G` or `N`
+        out_dict[specie]['ids'].add(id)
+
         valid_seq = True
-        #count = 0
         for c in seq:
             if c not in "ATCGN":
                 valid_seq = False
+
         if valid_seq:
-            #na verdade toal aqui!
             out_dict[specie]['gc_medio'] += (seq.count('G') + seq.count('C')) / len(seq)
             out_dict[specie]['count'] += 1
-            #count += 1 <- store in dict then delete value!
-    # ja vimos todas as sequencias!
+
     for specie in out_dict:
         if out_dict[specie]['count']> 0:
             out_dict[specie]['gc_medio'] = round(out_dict[specie]['gc_medio'] / out_dict[specie]['count'], 4)
         else:
             out_dict[specie]['gc_medio'] = None
         del out_dict[specie]['count']
-    
-    # DO search if theres a python funct that grabs substrings by chars
-        #re.search()<. usa uma sintax tipo sed(?ig)
-        # sim usa expressoes regulares (regex)
-        # \ para escapar [ e ] como caracteres especiais
+
     return out_dict
-# tem header com o formato bem, mas é proteina
-dict1 = indexSpeciesGC(readFASTA("data_sequences/NP_001138820.1.fasta"))
-print(dict1)
-dict2 = indexSpeciesGC(readFASTA("data_sequences/NP_001362750.1.fasta"))
-print(dict2)
-#ids_gc_medio  =  { }
-# {dict.values[0] : ids_gc_medio }
+
+
+# Tests:
+
+#T1
+# print(f"CY043485.1.fasta\n{readFASTA('data_sequences/CY043485.1.fasta')}")
+# d =readFASTA("data_sequences/U49845.1.fasta")
+# print(d)
+# for k, v in d.items(): # v- tuple # k-str
+#     print(type(k), type(v), len(v))
+
+# from Bio import SeqIO # problema: ModuleNotFoundError: No module named 'Bio'    solucao:  python -m pip install biopython   OU  pip install biopython 
+
+# bio_dict = {}
+# for record in SeqIO.parse("data_sequences/U49845.1.fasta", "fasta"):
+#     bio_dict[record.id] = (record.description[len(record.id):].strip(), str(record.seq))
+
+# my_dict = readFASTA("data_sequences/U49845.1.fasta")
+
+# print(my_dict == bio_dict)
+
+#T2
+# print(seqComposition('AGCCGTACGG', 'DNA'))
+# print(Counter('AGCCGTACGG')) # comparação de contagem
+# print(seqComposition('AGCCGTACGG', 'RNA'))
+# print(seqComposition('AGCXCGTA_CGGN', 'DNA')) # tem de dar igual ao primeiro teste
+# print(seqComposition('AGCXCG_NTACGG', 'NO')) # wriong type
+# print(seqComposition('ARNDCEQHILKMFPSTWYV', 'PROTEIN'))
+
+#T2
+# print(dnaGCcontent("AGCCGTACGG"))
+# print(dnaGCcontent("A"))
+# print(dnaGCcontent("G"))
+# print(dnaGCcontent("AGCCGXXTACGG")) # uso de SeqComposition caracteres maus, nao afetam GC content
+# from Bio.SeqUtils import gc_fraction
+# tool_gc = gc_fraction("AGCCGTACGG")
+# print(round(tool_gc * 100,2))
+
+#T3
+# dna_seq = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"
+# rna_seq = transcribeDNA2RNA(dna_seq)
+# print("DNA:", dna_seq)
+# print("RNA:", rna_seq)
+# dna_seq = "AAGGCC"
+# rna_seq = transcribeDNA2RNA(dna_seq)
+# print("DNA2:", dna_seq)
+# print("RNA2:", rna_seq)
+# dna_seq = "TTTTT"
+# rna_seq = transcribeDNA2RNA(dna_seq)
+# print("DNA3:", dna_seq)
+# print("RNA3:", rna_seq)
+# dna_seq = ""
+# rna_seq = transcribeDNA2RNA(dna_seq)
+# print("DNA4:", dna_seq)
+# print("RNA4:", rna_seq)
+# dna_seq = "AAG_TGCTXC" # nada diz sobre isto no enunciado portanto aqui vai so ignorar e retorna-los
+# rna_seq = transcribeDNA2RNA(dna_seq)
+# print("DNA5:", dna_seq)
+# print("RNA5:", rna_seq)
+
+#T4
+# nao parar quando encotra codao STOP
+
+# rna_seq = "AUGGCCAUUGUAAUGGGCCGCUGAAAGGGUGCCCGAUAG"
+# protein_seq = translateRNA2protein(rna_seq)
+# print("RNA:", rna_seq)
+# print("Protein:", protein_seq)
+# rna_seq = "AAGXXXXAUG"
+# # nada sobre bases erradas, so ignora mas aqui vejo como ignora tambem nao ter string multipla de tres (sem dar erro): ignora bases a mais no fim
+# protein_seq= translateRNA2protein(rna_seq)
+# print("RNA2:", rna_seq)
+# print("Protein2:", protein_seq)
+# rna_seq = ""
+# protein_seq = translateRNA2protein(rna_seq)
+# print("RNA3:", rna_seq)
+# print("Protein3:", protein_seq)
+
+# from Bio.Seq import Seq
+
+# rna_seq = "AUGGCCAUUGUAAUGGGCCGCUGAAAGGGUGCCCGAUAG"
+# print(Seq(rna_seq).translate())
+
+#T5
+# dna_seq = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCGATAG"
+# rev_complement = reverseComplement(dna_seq)
+# print("Reverse complement:", rev_complement)
+# from Bio.Seq import Seq
+# print(f"Reverse complement: {str(Seq(dna_seq).reverse_complement())}")
+# dna_seq = "ATcGAT"
+# rev_complement = reverseComplement(dna_seq)
+# print("Reverse complement2:", rev_complement)
+# dna_seq = "A"
+# rev_complement = reverseComplement(dna_seq)
+# print("Reverse complement3:", rev_complement)
+# dna_seq = "CG" # dar CG
+# rev_complement = reverseComplement(dna_seq)
+# print("Reverse complement4:", rev_complement)
+# dna_seq = "" # dar CG
+# rev_complement = reverseComplement(dna_seq)
+# print("Reverse complement5:", rev_complement)
+# dna_seq = "CXXG" # dar CG; ignora bases más devido ao uso direto do dict
+# rev_complement = reverseComplement(dna_seq)
+# print("Reverse complement6:", rev_complement)
+
+#T6
+# dna_seq = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"
+# motif = "GCC"
+# positions = findMotif(dna_seq, motif)
+# print(positions)
+# print(findMotif("GCCATG", "GCC"))
+# print(findMotif("ATGGCC", "GCC"))
+# print(findMotif("GCAATG", "GCC")) # nao existe
+# print(findMotif("AAAAA", "AAA")) # overlap
+# print(findMotif("GCCA", "GCCA"))
+# print(findMotif("GCC", "GCCC")) #empty list
+
+
+#T7
+# dna_seq = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"
+# k = 3
+# kmers = mostFrequentKMotifs(dna_seq, k)
+# print(kmers)
+# dna_seq2 = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCGATAG"
+# kmers2 = mostFrequentKMotifs(dna_seq2, k)
+# print(kmers2)
+# print(mostFrequentKMotifs("AAAAAA", 2))
+# print(mostFrequentKMotifs("ATATAT", 2))
+# print(mostFrequentKMotifs("AAGGTT", 1))
+# print(mostFrequentKMotifs("AAGGTT", 1))
+# print(mostFrequentKMotifs("ATG", 5))
+# print(mostFrequentKMotifs("", 3))
+# print(mostFrequentKMotifs("ATG", 3))
+#T8
+# #print(highestGC("data_sequences/J02459.1.fasta"))
+
+# #print(compositionMatrix("test.fasta"))
+
+# dict1 = indexSpeciesGC(readFASTA("data_sequences/NP_001138820.1.fasta"))
+# print(dict1)
+# dict2 = indexSpeciesGC(readFASTA("data_sequences/NP_001362750.1.fasta"))
+# print(dict2)
